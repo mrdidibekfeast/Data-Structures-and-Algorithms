2257893f05b7e735ab2486aeba05d7f08e789c9e
Marcus Wang <mrdidibekfeast88@gmail.com>
2026-04-19 16:02:17 -0700

    Recursive Triangles

---
+++ Recursion/SimpleRecursion/SimpleRecursion/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace SimpleRecursion
+{
+    internal class Program
+    {
+        public static void RightTrangle(int x)
+        {
+            if(x < 0)
+            {
+                return;
+            }
+
+            RightTrangle(x - 1);
+            while(x>0)
+            {
+                Console.Write("* ");
+                x--;
+            }
+            Console.WriteLine();
+        }
+
+        public static void IsocelesTriangle(int maxLevel, int currLevel = 0)
+        {
+            if(currLevel > maxLevel)
+            {
+                return;
+            }
+ 
+            int spaces = (((maxLevel *2)+1) / 2) - currLevel;
+            int stars = 1 + (currLevel * 2);
+
+            while(spaces > 0)
+            {
+                Console.Write("  ");
+                spaces--;
+            }
+            while(stars > 0)
+            {
+                Console.Write("* ");
+                stars--;
+            }
+            Console.WriteLine();
+
+            IsocelesTriangle(maxLevel, currLevel + 1);
+        }
+        public static void Main(string[] args)
+        {
+            RightTrangle(5);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            IsocelesTriangle(5);
+        }
+    }
+}
